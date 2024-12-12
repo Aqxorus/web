88db1b574dfa939f4da91bafccc9ba4bcf538ee7
evanqian2175 <evanqian2175@gmail.com>
2024-12-12 18:33:10 +1300

    feat: new description on about me and project website

--- src/components/sections/about/cozy/about.tsx
+++ src/components/sections/about/cozy/about.tsx
@@ -17,19 +17,14 @@ function About() {
             </h2>
             <div className="space-y-4">
               <p className="max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                <TextReveal>
-                  I am a passionate and creative Web Developer with a love for
-                  beautiful and functional websites. I have experience working
-                  with a variety of web technologies and frameworks and I am
-                  always eager to learn new things and take on new challenges.
-                </TextReveal>
+                <TextReveal>hey there 👋\nI'm a developer from NZ.</TextReveal>
               </p>
               <div className="flex gap-2">
-                <Button asChild variant={'outline'}>
+                {/* <Button asChild variant={'outline'}>
                   <a href="resume.pdf" target="_blank">
                     View Resume <ArrowUpRightIcon className="ml-2 size-5" />
                   </a>
-                </Button>
+                </Button> */}
                 <Button asChild>
                   <Link href="/about">Learn More</Link>
                 </Button>
